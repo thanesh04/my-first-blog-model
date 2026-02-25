429f749d588b99c154ecbe30d1e5acf886b856c4
Thanesh Maheshwari <thanesh.maheshwari2024@vitstudent.ac.in>
2026-02-25 13:00:40 +0530

    Added post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Home page (list of posts)
     path('', views.post_list, name='post_list'),
+
+    # Post detail page
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
+    #for like
+    path('like/<int:pk>/', views.like_post, name='like_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Post
 
+
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts_list = Post.objects.all().order_by('-published_date')
+
+    paginator = Paginator(posts_list, 5)
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def like_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.likes += 1
+    post.save()
+    return redirect('post_list')
